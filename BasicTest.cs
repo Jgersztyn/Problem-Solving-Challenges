37d76833fbcc1b127e96694c9c0fdfbd08b57b8c
jgersztyn <jgersztyn91@gmail.com>
2019-12-11 01:08:20 -0800

    Commit basic change making problem

---
+++ BasicTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingTests
+{
+    public class BasicTest
+    {
+        public int MakeChange(double amount)
+        {
+            if (amount <= 0)
+            {
+                return 0;
+            }
+
+            int result = 0;
+
+            while(amount > 0)
+            {
+                if(amount >= coins.GetValueOrDefault("Quarter"))
+                {
+                    result = result + 1;
+                    amount = amount - coins.GetValueOrDefault("Quarter");
+                }
+                else if (amount >= coins.GetValueOrDefault("Dime"))
+                {
+                    result = result + 1;
+                    amount = amount - coins.GetValueOrDefault("Dime");
+                }
+                else if (amount >= coins.GetValueOrDefault("Nickel"))
+                {
+                    result = result + 1;
+                    amount = amount - coins.GetValueOrDefault("Nickel");
+                }
+                else
+                {
+                    result = result + 1;
+                    amount = amount - coins.GetValueOrDefault("Penny");
+                }
+            }
+
+            return result;
+        }
+
+        public static Dictionary<string, double> coins = new Dictionary<string, double>()
+        {
+            { "Quarter", 0.25 },
+            { "Dime", 0.10 },
+            { "Nickel", 0.05 },
+            { "Penny", 0.01 }
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CodingTests
+{
+    class Program
+    {
+        // some old coding samples found here
+        // http://codercareer.blogspot.com/2011/10/no-15-fibonacci-sequences.html
+
+        static void Main(string[] args)
+        {
+            // value to pass into the function
+            double dollarAmount = 1.09;
+
+            // Instantiate class
+            BasicTest testInstance = new BasicTest();
+
+            // Execute code from given test
+            int minimumCoins = testInstance.MakeChange(dollarAmount);
+
+            // Show result
+            if(minimumCoins > 0)
+            {
+                Console.WriteLine("Minimum number of coins to make change is " + minimumCoins);
+            }
+            else
+            {
+                Console.WriteLine("No change required.");
+            }         
+        }
+    }
+}
